fc835017f00ff8035491129cb9ca307162f4bdb5
Ravi B <ravi@wfaglobal.com>
2026-04-10 19:39:48 -0400

    Dynamic copyright year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import styles from './Footer.module.css'
 
 export default function Footer() {
+  const year = new Date().getFullYear()
   return (
     <footer className={styles.footer}>
       <div className={styles.footerInner}>
@@ -10,7 +11,7 @@ export default function Footer() {
           <span>WFA Global Investments</span>
         </div>
         <div className={styles.fd}>
-          The content provided on this website is for informational purposes only and does not constitute investment advice, a solicitation, or a recommendation to buy or sell any securities. © 2026 WFA Global®
+          The content provided on this website is for informational purposes only and does not constitute investment advice, a solicitation, or a recommendation to buy or sell any securities. © {year} WFA Global®
         </div>
         <div className={styles.fr}>WFA Global Investments</div>
       </div>
